624770ca5f5d3178e978533ed165baab5e9014c7
Louis903995 <louis@moises.fr>
2025-08-29 12:09:32 +0200

    ajout du nom de l'utilisateur connecté

--- src/app.py
+++ src/app.py
@@ -4,48 +4,48 @@ from page_budgets import page_budgets
 from page_tickets import page_tickets
 from upload_page import page_upload
 from courbe_page import page_courbe
-
-load_dotenv(dotenv_path=".env", override=False)
-
-
+import os 
 import requests
 
-API_BASE_URL = "http://localhost:8501"  
 
-def api_authenticate(email: str):
-    """Récupère un client par email """
-    response = requests.get(f"{API_BASE_URL}/clients")  
-    if response.status_code == 200:
-        clients = response.json()
-        for client in clients:
-            if client["email_client"] == email:
-                return client
-    return None
+load_dotenv(dotenv_path=".env", override=False) 
+
+INGESTION_SERVICE_URL = os.environ["INGESTION_SERVICE_URL"]
+
+
+
+
+# def api_authenticate(email: str):
+#     """Récupère un client par email """
+#     response = requests.get(f"{INGESTION_SERVICE_URL}/clients")  
+#     if response.status_code == 200:
+#         clients = response.json()
+#         for client in clients:
+#             if client["email_client"] == email:
+#                 return client
+#     return None
 
 
 def fake_azure_auth():
     if "authenticated" not in st.session_state:
         st.session_state["authenticated"] = False
-
     if not st.session_state["authenticated"]:
         st.title("Authentification requise")
         with st.form("auth_form"):
-            email = st.text_input("Email")
-            password = st.text_input("Mot de passe", type="password")  # optionnel pour la démo
+            username = st.text_input("Nom d'utilisateur Azure")
+            password = st.text_input("Mot de passe", type="password")
             submit = st.form_submit_button("Se connecter")
-
             if submit:
-                if email and password:
-                    client = api_authenticate(email)
-                    if client:
+                if username and password:
+                    response = requests.get(f"{INGESTION_SERVICE_URL}/clients/{username}")  
+                    if response.status_code == 200:
+                        st.session_state["client"] = response.json()
                         st.session_state["authenticated"] = True
-                        st.session_state["client"] = client
-                        st.success(f"Authentifié en tant que {client['prenom_client']} {client['nom_client']}")
+                        st.session_state["username"] = username
+                        st.success("Authentifié !")
                         st.rerun()
-                    else:
-                        st.error("Client introuvable.")
                 else:
-                    st.error("Veuillez remplir tous les champs.")
+                    st.error("Identifiants invalides.")
         st.stop()
 
 def page_logout():
